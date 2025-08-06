0c8e850596d02ff8a2ee4b072ba49fad95be7f4c
Windyphan <thandongdatviettatca@gmail.com>
2025-08-06 21:17:32 +0700

    add initial project structure and configuration files

---
+++ client/src/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AppProvider } from './context/AppContext';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Browse from './pages/Browse';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Library from './pages/Library';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import TrackUpload from './pages/admin/TrackUpload';
+import MusicPlayer from './components/MusicPlayer';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminRoute from './components/AdminRoute';
+import './App.css';
+
+function App() {
+  return (
+    <AppProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/browse" element={<Browse />} />
+              <Route path="/cart" element={
+                <ProtectedRoute>
+                  <Cart />
+                </ProtectedRoute>
+              } />
+              <Route path="/checkout" element={
+                <ProtectedRoute>
+                  <Checkout />
+                </ProtectedRoute>
+              } />
+              <Route path="/dashboard" element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              } />
+              <Route path="/library" element={
+                <ProtectedRoute>
+                  <Library />
+                </ProtectedRoute>
+              } />
+              <Route path="/admin" element={
+                <AdminRoute>
+                  <AdminDashboard />
+                </AdminRoute>
+              } />
+              <Route path="/admin/upload" element={
+                <AdminRoute>
+                  <TrackUpload />
+                </AdminRoute>
+              } />
+            </Routes>
+          </main>
+          <Footer />
+          <MusicPlayer />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
+      </Router>
+    </AppProvider>
+  );
+}
+
+export default App;
